3c795bba1dae8de7e5adcdfe5b6660f337bb7849
DanielaCastorena <77950260+DanielaCastorena@users.noreply.github.com>
2024-02-05 01:10:53 -0800

    Add files via upload

---
+++ as0/README.md
@@ -0,0 +1,23 @@
+#381raylib - Assignment 0
+
+This project demonstrates a simple text animation using the Raylib library.
+
+##Git Submodules
+
+###How to Fetch Git Submodules
+
+To fetch Git submodules for this project, execute the following command:
+
+```bash
+git submodule update --init --recursive
+
+#initialize and update submodules
+git submodule update --init --recursive
+g++ -o as0 main.cpp -lraylib -std=c++11
+
+#raylib must be installed!
+#to compile code:
+"g++ -o as0 main.cpp -lraylib -std=c++11"
+
+#after compiling, run the executable with:
+"./as0"
--- as0/main.cpp
+++ as0/main.cpp
@@ -1,1 +1,76 @@
+#include "raylib.h"
+#include <cmath>
+
+const char* textToDisplay = "Hi, my name is Daniela!";
+
+//changing colors function
+Color LerpColor(Color a, Color b, float t) {
+    return (Color){
+        (unsigned char)(a.r + (b.r - a.r) * t),
+        (unsigned char)(a.g + (b.g - a.g) * t),
+        (unsigned char)(a.b + (b.b - a.b) * t),
+        (unsigned char)(a.a + (b.a - a.a) * t)
+    };
+}
+
+int main(){
+    int screenWidth = 800;
+    int screenHeight = 600;
+
+    //window resizing
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+
+    InitWindow(screenWidth, screenHeight, "CS381 - Assignment 0");
+    SetTargetFPS(60);
+
+    //ttf font & convert it to raylib font
+    Font font = LoadFontEx("cursive.ttf", 32, 0, FONT_BITMAP);
+    SetTextureFilter(font.texture, TEXTURE_FILTER_BILINEAR);
+
+    //background image
+    Texture2D background = LoadTexture("hellokitty.jpeg"); 
+
+    float textScale = 20.0f;
+
+    while (!WindowShouldClose()){
+        if (IsWindowResized()){
+            screenWidth = GetScreenWidth();
+            screenHeight = GetScreenHeight();
+            SetWindowSize(screenWidth, screenHeight);
+        }
+
+        //extra credit! text animation
+        textScale = 20.0f + 5.0f * sinf(GetTime());
+
+        //center text on screen
+        Vector2 textSize = MeasureTextEx(font, textToDisplay, textScale, 0);
+        Vector2 textPosition = {(float)(screenWidth - textSize.x) / 2, (float)(screenHeight - textSize.y) / 2};
+
+        //extra credit! change text color between 3 colors 
+        float t = 0.5f + 0.5f * sinf(GetTime()); 
+        Color textColor = LerpColor(LerpColor(PURPLE, PINK, t), BLUE, t);
+
+        BeginDrawing();
+        
+        //background image
+        DrawTexture(background, 0, 0, WHITE);
+
+        ClearBackground(BLACK);
+
+        //ttf font
+        DrawTextEx(font, textToDisplay, textPosition, textScale, 0, textColor);
+
+        EndDrawing();
+    }
+
+    UnloadFont(font);
+    UnloadTexture(background);
+
+    CloseWindow();
+    return 0;
+}
+
+
+
+
 
