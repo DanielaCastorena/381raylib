3e67b364985704a6b9f4a2eacce0183bb4bcdfe4
Daniela Castorena <dani.castorena7@gmail.com>
2024-03-10 22:57:05 -0700

    as5

---
+++ as5/build/CMakeFiles/as5.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for as5.
+# This may be replaced when dependencies are built.
---
+++ as5/src/raygui.h
@@ -0,0 +1,6 @@
+#ifndef RAYGUI_H
+#define RAYGUI_H
+
+// Define necessary structs, enums, and function prototypes for Raygui here
+
+#endif // RAYGUI_H
